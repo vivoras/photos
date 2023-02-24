7d5e3456058369b5f7ac9f253c88f51bdd197d80
John Molakvoæ <skjnldsv@protonmail.com>
2023-02-24 12:02:22 +0100

    fix(composer): autoloader
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

---
+++ composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nextcloud/photos',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '77df935b7b6aba082a000f025bfdf29876a6acbd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'hexogen/kdtree' => array(
+            'pretty_version' => 'v0.2.5',
+            'version' => '0.2.5.0',
+            'reference' => 'f739186638445990463762d467e07a8262228daa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../hexogen/kdtree',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nextcloud/photos' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '77df935b7b6aba082a000f025bfdf29876a6acbd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
